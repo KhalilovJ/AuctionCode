22b07dbb0cc885d3d993fbe9a7a11586ff03b494
Khalilov157 <evilcrewstudios@gmail.com>
2023-03-10 14:29:34 +0400

    redirecting when logged in

--- AuctionBackend/src/main/java/az/code/auctionbackend/controllers/SecurityController.java
+++ AuctionBackend/src/main/java/az/code/auctionbackend/controllers/SecurityController.java
@@ -1,5 +1,8 @@
 package az.code.auctionbackend.controllers;
 
+import az.code.auctionbackend.entities.users.UserProfile;
+import az.code.auctionbackend.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -9,16 +12,45 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SecurityController {
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @GetMapping("/login")
-    String login() {
-        return "login";
+    public ModelAndView login(@AuthenticationPrincipal UserDetails user) {
+        ModelAndView nextPage;
+        if (user != null){
+            nextPage = new ModelAndView("index");
+            nextPage.addObject("user", user);
+        } else {
+            nextPage = new ModelAndView("login");
+        }
+
+        return nextPage;
+    }
+
+    @GetMapping("/home")
+    public ModelAndView getHome(@AuthenticationPrincipal UserDetails user){
+        ModelAndView nextPage = null;
+
+        UserProfile userProfile = userService.findByUsername(user.getUsername()).orElse(null);
+
+        if (userProfile.getRole().getName().equals("USER") ){
+            nextPage = new ModelAndView("index");
+        } else {
+            nextPage = new ModelAndView("adminPanel");
+        }
+
+        nextPage.addObject("user", user);
+        return nextPage;
     }
 
     @GetMapping("/index")
-    public ModelAndView getHome(@AuthenticationPrincipal UserDetails user){
+    public ModelAndView getIndex(@AuthenticationPrincipal UserDetails user){
         ModelAndView nextPage = new ModelAndView("index");
 
         nextPage.addObject("user", user);
         return nextPage;
     }
+
+
 }
